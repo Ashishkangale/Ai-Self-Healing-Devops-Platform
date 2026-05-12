a9f4b7b18a648b4a42e3f8ff783ee5bc73ddca79
Ashish Kangale <kangaleashish68@gmail.com>
2026-05-12 13:24:19 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,13 +13,11 @@ variable "instance_type" {
 variable "public_key_path" {
   description = "Path to your SSH public key file"
   type        = string
-  # Windows users: change this to your full path, e.g.:
-  # default = "C:/Users/kanga/.ssh/id_rsa.pub"
   default     = "C:/Users/kanga/.ssh/id_rsa.pub"
 }
 
 variable "allowed_cidr" {
   description = "Your IP address for SSH access (find at https://whatismyip.com, add /32)"
   type        = string
-  default     = "0.0.0.0/0"   # ← Change to YOUR_IP/32 for security, e.g. "103.45.67.89/32"
+  default     = "0.0.0.0/0"   
 }
